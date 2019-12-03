b9d81b295b5ce27ecc3aa7093d8855c74ce6c551
shrivatsk <50849898+shrivatsk@users.noreply.github.com>
2019-12-03 09:05:14 -0500

    Delete isAuthenticated.js

--- middlewares/isAuthenticated.js
+++
@@ -1,9 +0,0 @@
-var isAuthenticated = function (req, res, next) {
-  if (req.session.user && req.session.user.length > 0) {
-    next()
-  } else {
-    res.redirect('/login')
-  }
-}
-
-module.exports = isAuthenticated;
